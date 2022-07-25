ea8fdc035c8bb884b34b1eeb279ce4dabdad96c1
Wang Lun <wanglun02@gmail.com>
2022-07-25 18:23:20 +0900

    record

--- Package.swift
+++ Package.swift
@@ -16,7 +16,8 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/ReactiveX/RxSwift.git", .exact("6.5.0")),
         .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.6.1")),
-        .package(path: "SwiftyJSON")
+        .package(path: "SwiftyJSON"),
+        .package(url: "https://github.com/realm/realm-swift", branch: "master")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
